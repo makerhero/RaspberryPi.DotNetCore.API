b8b0a27ac9628d2eb4cd0544d40a7c5ec81ffcf8
Alliston Carlos Rosa Aleixo <alliston@outlook.com>
2018-03-29 08:15:51 -0300

    Exposição da aplicação publicamente na rede

--- RaspberryPi.API/Program.cs
+++ RaspberryPi.API/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace RaspberryPi.API
 {
@@ -19,6 +12,7 @@ namespace RaspberryPi.API
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:5000")
                 .UseStartup<Startup>()
                 .Build();
     }
